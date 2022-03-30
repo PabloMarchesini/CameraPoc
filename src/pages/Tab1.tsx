eb2bd4c71b81b5c58a73406cdeac15d7afeae29c
Pablo Marchesini <pablo.marchesini@emerios.com>
2022-03-30 17:13:57 -0300

    first commit

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,24 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useEffect, useRef, useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+
+  // const [videoStream, setVideoStream] = useState({});
+  const videoStream = useRef<HTMLVideoElement>(null!);
+
+  useEffect(() => {
+    const getMedia = async () => {
+      const stream = await navigator.mediaDevices.getUserMedia({audio: false, video: true});
+      // setVideoStream(stream);
+      videoStream.current.srcObject = stream;
+      
+    }
+
+    getMedia();
+  })
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +32,8 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <video autoPlay={true} ref={videoStream} id="gum-local" ></video>
+        {/* <ExploreContainer name="Tab 1 page" /> */}
       </IonContent>
     </IonPage>
   );
